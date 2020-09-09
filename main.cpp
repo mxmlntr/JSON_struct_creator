22b8853325bef9478f4e316d847f73237ba782a0
vecxim <Maximilian.Rentschler@vector.com>
2020-09-09 14:37:25 +0200

    Added comments.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/**        \file  /home/visxim/CLionProjects/JSON_struct_creator/main.cpp
+ *        \brief  main of the JSON_struct_creator
+ *
+ *      \details This project is for generating JSON files and the neccessary code (struct, archive, CRC, parse)
+ *      to run it. The size can be predefined by the number of cycles.
+ *      ATTENTION: These generated JSON files are with the only purpose of simulating different sizes JSON files,
+ *      they depend on a repetitive algorithm.
+ *
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  INCLUDES
+ *********************************************************************************************************************/
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -9,9 +29,11 @@ uint32_t str_obj_appe_itr = 0;
 
 uint16_t str_val_itr = 0;
 
+//! Definition of the String which will define the JSON file structure
 string object_names[10] = {"name","description","mode","version","status","path","buffer_size","id","number","length"};
 string object_values[10] = {"JSON_creator","dummy json","shutdown","V1.283","running","~/etc/tmp/process1/running","2048","1337","1","2309763"};
 
+//! resets the Values of the iterators
 void resetValues()
 {
     str_obj_itr = 0;
@@ -19,6 +41,7 @@ void resetValues()
     str_val_itr = 0;
 };
 
+//! returns the next name from the object_name array
 string get_string_object()
 {
     if(str_obj_itr == 10)
@@ -29,6 +52,7 @@ string get_string_object()
    return buf.append(to_string(str_obj_appe_itr++));
 }
 
+//! returns the next value from the object_value array
 string get_string_value()
 {
     if(str_val_itr == 10)
@@ -39,6 +63,7 @@ string get_string_value()
     return buf;
 }
 
+//! Function for creating the synthetic JSON file with the given parameters
 uint8_t createJSON(uint16_t length)
 {
     ofstream jsn("/home/visxim/CLionProjects/ShareFolder/EXMPLE.json");
@@ -77,6 +102,7 @@ uint8_t createJSON(uint16_t length)
     return 0;
 };
 
+//! Function for creating the struct for the matching JSON file
 uint8_t createStruct(uint16_t length)
 {
     ofstream strct("createdStruct.cpp");
@@ -105,6 +131,7 @@ uint8_t createStruct(uint16_t length)
     return 0;
 };
 
+//! Function for creating the Archive for the matching JSON file
 uint8_t createArchive(uint16_t length)
 {
     ofstream arc("createdArchive.cpp");
@@ -124,7 +151,7 @@ uint8_t createArchive(uint16_t length)
     resetValues();
     return 0;
 };
-
+//! Function for creating the CRC for the matching JSON file
 uint8_t createCRC(uint16_t length)
 {
     ofstream crc("createdCRC.cpp");
@@ -140,6 +167,7 @@ uint8_t createCRC(uint16_t length)
     crc << "return crc.get();\n}\n";
 };
 
+//! Function for creating the parse information for the matching JSON file
 uint16_t createParse(uint16_t length)
 {
     ofstream prs("createdParse.cpp");
@@ -160,10 +188,12 @@ uint16_t createParse(uint16_t length)
     return 0;
 };
 
+//! main
 int main()
 {
     //! Declaration of the size of the JSON file 100 ~ 2,5kb
     uint16_t size = 10000;
+    //! Creating all neccessary objects NOTICE: JSON file will be saved to ShareFolder
     createJSON(size);
     createStruct(size);
     createArchive(size);
